d38fe9fc5cf94cf002e8bd59e082c86070a7960f
Stefano Palomino <stefanop21@outlook.es>
2024-08-12 11:12:48 -0500

    feat: new ticket screen

--- public/js/new-ticket.js
+++ public/js/new-ticket.js
@@ -1,1 +1,17 @@
-console.log("Nuevo Ticket HTML");
+const span = document.getElementById("lbl-new-ticket");
+const button = document.querySelector("button");
+
+async function getLastTicket() {
+  const response = await fetch("api/ticket/last");
+  span.innerText = await response.json();
+}
+
+async function createTicket() {
+  const data = await fetch("api/ticket", {
+    method: "POST",
+  }).then((response) => response.json());
+  span.innerText = data.ticketNumber;
+}
+
+button.addEventListener("click", createTicket);
+getLastTicket();
